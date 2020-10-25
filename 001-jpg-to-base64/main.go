d6e9e7d4997ba845f194f3f4871d120e3105b981
cherrybongbong <cherrybongbong@cherrybongbongui-MacBookPro.local>
2020-10-25 16:36:54 +0900

    Add 001 & 002

---
+++ 001-jpg-to-base64/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"bufio"
+	"encoding/base64"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+func main() {
+	f, _ := os.Open("./tesla.jpeg")
+
+	// Read entire JPG into byte slice.
+	reader := bufio.NewReader(f)
+	content, _ := ioutil.ReadAll(reader)
+
+	// Encode as base64.
+	encoded := base64.StdEncoding.EncodeToString(content)
+
+	// Print encoded data to console.
+	// ... The base64 image can be used as a data URI in a browser.
+	fmt.Println("ENCODED: " + encoded)
+}
